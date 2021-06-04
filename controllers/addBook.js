4b62deacc0f29ee1f5ee05a344738c2e1f139a1e
Allyssa <a.alimoestar@gmail.com>
2021-06-04 11:55:57 +0200

    added addBook feature
    wip

---
+++ controllers/addBook.js
@@ -0,0 +1,34 @@
+const express = require('express')
+// middleware voor endpoints. nodig om dit extern
+const router = express.Router()
+
+const mongoose = require('mongoose')
+const boek = require('../schema/boek.schema')
+const gebruiker = require('../schema/gebruiker.schema')
+
+
+// collection, schema
+const newBoek = mongoose.model('book', book)
+
+
+// functie die data pakt en deze opslaat in mongoose
+function saveData(data) {
+  let newBook = new newBoek({
+      titel: data.titel,
+      auteur: data.auteur,
+      genre: data.genre,
+      entryDate: Date()
+  })
+
+  newBook.save((err) => {
+    console.log(`${newBook}`)
+    if (err) return handleError(err)
+  })
+}
+
+// functie die boekenlijst ophaalt
+async function getBooks() {
+  // lean() transforms mongoose object to json object
+  const data = await newBoek.find().lean()
+  return data
+}
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require("express");
-const chalk = require("chalk");
-require("dotenv").config();
+const express = require('express');
+const chalk = require('chalk');
+require('dotenv').config();
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Connecting mongoose
-const connectDBMongoose = require("./controllers/mongoose");
+const connectDBMongoose = require('./models/mongoose');
 connectDBMongoose();
 
 // Home route
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
 // Booting app
-app.listen(process.env.PORT, () => {
-    console.log(chalk.blueBright(`Example app listening at http://localhost:${process.env.PORT}`));
+app.listen(port, () => {
+    console.log(chalk.blueBright(`Example app listening at http://localhost:${port}`));
 });
