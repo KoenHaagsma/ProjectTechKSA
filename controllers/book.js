42ff50d6708bf93e6e8c5c71391040b3b62e830d
Koen Haagsma <koenhaagsma123@hotmail.nl>
2021-06-10 11:37:16 +0200

    Fixed book.js model

--- controllers/book.js
+++ controllers/book.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const book = new Schema({
-    id: Schema.Types.ObjectId,
-    titel: String,
-    auteur: String,
+    title: String,
+    author: String,
     genre: String,
     entryDate: Date,
 });
