5ec77a53d02e50d70625c122ed40a94ea57a1820
Allyssa <a.alimoestar@gmail.com>
2021-06-16 11:20:47 +0200

    added option to delete a book

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -247,6 +247,23 @@ router.get('/myProfile', async (req, res) => {
     }
 });
 
+// Delete a book
+router.post('/deleteBook', async (req, res) => {
+    try {
+        Book.deleteOne({
+            _id : req.body.id,
+        })
+        await Book.deleteOne()
+        res.render('myProfile', {
+            books: await getBooks()
+        })
+    } catch (error) {
+        if (err) return console.log(err)
+    }
+    
+})
+
+
 // Matching feature
 // Discover new books
 router.get('/ontdekken', (req, res) => {
